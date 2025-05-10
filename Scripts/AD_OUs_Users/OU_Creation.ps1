ff7c983c39d624b20bd55700e61ce9029d6f97a0
roberta-vieira <robertasofia99@hotmail.com>
2025-05-10 19:32:56 +0100

    Add files via upload

---
+++ Scripts/AD_OUs_Users/OU_Creation.ps1
@@ -0,0 +1,21 @@
+﻿# Description> This script is used for creating OU and the users in the respective OUs.
+
+# Import active directory module for running AD cmdlets
+Import-Module activedirectory
+
+# Store the data from the CSV file that contains OU information
+$ous = Import-Csv ".\import_ou.csv" -Delimiter ';'
+
+Write-Output "\nCreating OUs in Active Directory"
+Write-Output $ous
+
+# Loop through each line of the CSV line and creation of OU in AD
+foreach ($ou in $ous){
+    $name = $ou.name
+    $path = $ou.path
+
+    New-ADOrganizationalUnit -Name $name -Path $path -ProtectedFromAccidentalDeletion $False
+
+}
+
+
---
+++ Scripts/AD_OUs_Users/User_Creation.ps1
@@ -0,0 +1,24 @@
+﻿# Import Active Directory modules to use AD cmdlets
+Import-Module ActiveDirectory
+
+# Import user data information from the CSV file into a variable
+$users = Import-Csv ".\import_users.csv" -Delimiter ";"
+
+# Default password for the users
+$password = ConvertTo-SecureString "1Df@ultP@ssword!Team-A" -AsPlainText -Force
+
+# Loop through each row to gather the user's information and create the AD account
+foreach ($user in $users){
+
+    Write-Output "User being created:"
+    Write-Output $user
+
+    $first_name = $user.'First Name'
+    $last_name = $user.'Last Name'
+    $job_title = $user.’Job Title’
+    $department = $user.'Department'
+    $OU_path = $user.'Organizational Unit'
+
+    # Create a new AD user with the information from the CSV file
+    New-ADUser -Name "$first_name $last_name" -GivenName $first_name -Surname $last_name -UserPrincipalName "$first_name.$last_name" -SamAccountName "$first_name.$last_name" -Title $job_title -Department $department -Path $OU_path -AccountPassword $password -ChangePasswordAtLogon $True -Enabled $True
+}
